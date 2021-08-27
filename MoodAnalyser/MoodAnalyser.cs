ff46f5c8e623dca7822b7f22f7cb77f60c87415c
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-27 11:40:43 +0530

    MoodAnalyserFactory

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        public MoodAnalyser() { }
+        public string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood Should Not Be Empty");
+                }
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not Be Null");
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace MoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            //Act
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string mood = moodAnalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+        /// <summary>
+        /// TC 1.1 & 2.1
+        /// </summary>
+        /// <param name="message"></param>
+        [TestMethod]
+        [DataRow("I am in HAPPY Mood")]
+        [DataRow(null)]
+        public void GivenHAPPYMoodShouldReturnHappy(string message)
+        {
+            //Arrange
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyser.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+        /// <summary>
+        /// TC3.2: Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indication_EmptyMood.
+        /// </summary>
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indication_EmptyMood()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string mood = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                Assert.AreEqual("Mood Should Not Be Empty", e.Message);
+            }
+        }
+        /// <summary>
+        /// TC3.1: Given Null Mood Should Throw MoodAnalysisException.
+        /// </summary>
+        [TestMethod]
+        public void Given_Null_Mood_Should_Throw_MoodanalysisException()
+        {
+            try
+            {
+                string message = null;
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string mood = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                Assert.AreEqual("Mood Should Not Be Null", e.Message);
+            }
+        }
+        /// <summary>
+        /// Tc 4.1 Mood Analyser Obj Should Return MoodAnalyser Object
+        /// </summary>
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyserFactory(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyzer.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+
+    }
+}
